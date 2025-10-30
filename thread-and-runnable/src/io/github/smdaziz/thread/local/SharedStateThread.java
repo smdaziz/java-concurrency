bb9fb7fe26754bb1d5fd98ff15290a409617076a
Abdul <smdaziz@gmail.com>
2025-10-29 20:52:46 -0500

    thread local examples showing shared and thread specific state

---
+++ thread-and-runnable/src/io/github/smdaziz/thread/local/SharedStateThread.java
@@ -0,0 +1,29 @@
+package io.github.smdaziz.thread.local;
+
+public class ThreadSharedStateDemo {
+    public static void main(String[] args) {
+        NamedThread namedThread = new NamedThread();
+        Thread t1 = new Thread(namedThread, "NamedThread-1");
+        Thread t2 = new Thread(namedThread, "NamedThread-2");
+        Thread t3 = new Thread(namedThread, "NamedThread-3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class NamedThread implements Runnable {
+    private String threadName;
+
+    @Override
+    public void run() {
+        threadName = Thread.currentThread().getName();
+        System.out.println(threadName + " started");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println(threadName + " finished");
+    }
+}
